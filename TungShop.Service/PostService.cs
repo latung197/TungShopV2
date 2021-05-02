97db054f2879f566a728ff27ec8a0d587054d907
DESKTOP-9F5APHK\Admin <levantung0000@gmail.com>
2021-05-02 13:03:10 +0700

    Bai9

---
+++ TungShop.Service/PostService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TungShop.Model.Model;
+using TungShop.Data.Repositorys;
+using TungShop.Data.Infastructure;
+
+namespace TungShop.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetAllPagin(int page, int pageSize, int totalRow);
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow);
+        IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
+        Post GetById(int id);
+        void SaveChanges();
+
+    }
+    public class PostService : IPostService
+    {
+        IPostRepository _postRepository;
+        IUnitOfwork _unitOfwork;
+        public PostService(IPostRepository postRepository, IUnitOfwork unitOfwork)
+        {
+            this._postRepository = postRepository;
+            this._unitOfwork = unitOfwork;
+        }
+        public void Add(Post post)
+        {
+            _postRepository.Add(post);
+        }
+
+        public void Delete(int id)
+        {
+            _postRepository.Delete(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _postRepository.GetAll(new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryId, out totalRow, page, pageSize, new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public IEnumerable<Post> GetAllPagin(int page, int pageSize, int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public Post GetById(int id)
+        {
+            return _postRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfwork.Commit();
+        }
+
+        public void Update(Post post)
+        {
+            _postRepository.Update(post);
+        }
+    }
+}
